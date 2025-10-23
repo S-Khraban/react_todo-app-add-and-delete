bcb18a39fa3c6892736359c08b0240d30be91127
S-Khraban <s.hraban@i.ua>
2025-10-23 13:11:41 +0300

    refactor: split TodoList item markup into reusable TodoItem component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,5 @@
-import cn from 'classnames';
 import type { Todo } from '../types/Todo';
+import { TodoItem } from './TodoItem';
 
 type Props = {
   todos: Todo[];
@@ -13,58 +13,20 @@ export const TodoList: React.FC<Props> = ({
   tempTodo,
   pendingIds,
   onDelete,
-}) => (
-  <section className="todoapp__main" data-cy="TodoList">
-    {[...todos, ...(tempTodo ? [tempTodo] : [])].map(todo => {
-      const isTemp = todo.id === 0;
-      const isPending = pendingIds.has(todo.id);
+}) => {
+  const items = [...todos, ...(tempTodo ? [tempTodo] : [])];
 
-      return (
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {items.map(todo => (
         <div key={todo.id}>
-          <div
-            data-cy="Todo"
-            className={cn('todo item-enter-done', {
-              completed: todo.completed,
-            })}
-          >
-            <label
-              className="todo__status-label"
-              aria-label="Toggle todo status"
-            >
-              <input
-                data-cy="TodoStatus"
-                type="checkbox"
-                className="todo__status"
-                checked={todo.completed}
-                readOnly
-              />
-            </label>
-
-            <span data-cy="TodoTitle" className="todo__title">
-              {todo.title}
-            </span>
-
-            {!isTemp && (
-              <button
-                type="button"
-                className="todo__remove"
-                data-cy="TodoDelete"
-                onClick={() => onDelete(todo.id)}
-              />
-            )}
-
-            <div
-              data-cy="TodoLoader"
-              className={cn('modal overlay', {
-                'is-active': isTemp || isPending,
-              })}
-            >
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
-            </div>
-          </div>
+          <TodoItem
+            todo={todo}
+            isPending={pendingIds.has(todo.id)}
+            onDelete={onDelete}
+          />
         </div>
-      );
-    })}
-  </section>
-);
+      ))}
+    </section>
+  );
+};
